11dca97f2e3be8b40edb26da05ed8fa6a31addcc
Ginko <piotr_czajka@outlook.com>
2016-04-01 23:05:04 +0200

    Added some i/o

--- linkedlist/main.c
+++ linkedlist/main.c
@@ -9,7 +9,7 @@ typedef struct element{
 
 typedef struct linked_list{
 	element *start;
-	int *count;
+	int count;
 }  ll;
 
 ll* create_ll(){
@@ -20,7 +20,6 @@ ll* create_ll(){
 	
 	ll *linked_list = (ll*)malloc(sizeof(ll));
 	linked_list->start=head;
-	linked_list->count=(int*)malloc(sizeof(int));
 	linked_list->count=0;
 	return linked_list;
 }
@@ -28,22 +27,26 @@ ll* create_ll(){
 
 void print_ll(ll *linked_list){
 	puts("");
+	int index=0;
 	element* temp=linked_list->start;
-	if (temp->next!=NULL)
+	if (temp->next!=NULL){
 			temp=linked_list->start->next;
+	}
 	if (temp->next==NULL && temp->prev==NULL){
 		
 		printf("There is no elements"); return;
 		}
 	while (temp->next!=NULL){
-		printf("%d, ", temp->value);
+		printf("ll[%d]=%d\n", index, temp->value);
+		index++;
 		temp=temp->next;
 	}
 	if (temp->prev!=NULL)
-		printf("%d, ", temp->value);
+		printf("ll[%d]=%d\n", index, temp->value);
 }
 
 void print_ll_bck(ll *linked_list){
+	int index=0;
 	element *temp = linked_list->start;
 	if (temp->next!=NULL)
 			temp=linked_list->start->next;
@@ -53,9 +56,11 @@ void print_ll_bck(ll *linked_list){
 		}
 	while(temp->next!=NULL){
 		temp=temp->next;
+		index++;
 	}
 	while (temp->prev!=NULL){
-		printf("%d, ", temp->value);
+		printf("ll[%d]=%d\n", index, temp->value);
+		index--;
 		temp=temp->prev;
 	}
 }
@@ -75,20 +80,27 @@ void add_node_after_inx(ll* linked_list, int value, int index){
 		node->value=value;
 		if (node->next!=NULL)
 			node->next->prev=node;
-		*(linked_list->count)++;
+		linked_list->count++;
 }
 
 
 
 int main(){
 	ll* linked_list = create_ll();
-	add_node_after_inx(linked_list, 1, 1);
-	add_node_after_inx(linked_list, 2, 2);
-	add_node_after_inx(linked_list, 3, 3);
-	add_node_after_inx(linked_list, 4, 4);
-	add_node_after_inx(linked_list, 5, 2);
-	print_ll(linked_list);
-	puts("");
-	print_ll_bck(linked_list);
+	int choose, val, idx;
+	while(1==1){
+		puts("What do you want to do? 1-add an element, 2-print elements 3-print backwards: ");
+		scanf("%d", &choose);
+		switch(choose){
+			case 1:
+				printf("Input value of element and index you want to put it after: ");
+				scanf("%d %d", &val, &idx);
+				add_node_after_inx(linked_list, val, idx);
+				break;
+			case 2: print_ll(linked_list); printf("\n"); break;
+			case 3: print_ll_bck(linked_list); printf("\n"); break;
+			default: printf("Wrong choice\n");
+		}
+	}
 }
 
