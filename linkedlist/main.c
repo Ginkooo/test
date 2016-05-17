ec90a17aa9ed85adc4c3284b5ad262f935142d34
Ginkooo <Ginkooo@users.noreply.github.com>
2016-05-17 15:28:28 +0200

    New awesome features

--- linkedlist/main.c
+++ linkedlist/main.c
@@ -65,10 +65,32 @@ void print_ll_bck(ll *linked_list){
 	}
 }
 
-void add_node_after_inx(ll* linked_list, int value, int index){
-	index++;
+void del_node_at_inx(ll* linked_list, int index){
+	if (index<0) {printf("Index must ba grater or equal 0"); return; }
+	int ix;
 	element* temp=linked_list->start;
-	if (index<=0) { printf("Error: index must be greater than 0"); fflush(stdout); return; }
+	for (ix=0; ix<=index; ix++){
+	if (temp->next!=NULL)
+		temp=temp->next;
+	if (temp->next==NULL && ix<index)
+		{
+		printf("There is no such an index");
+		return;
+		}
+	}
+	if (temp->next!=NULL){
+		temp->next->prev=temp->prev;
+		temp->prev->next=temp->next;
+		}
+	else
+		temp->prev->next=NULL;
+	free(temp);
+}
+
+void add_node_at_inx(ll* linked_list, int value, int index){
+	
+	element* temp=linked_list->start;
+	if (index<0) { printf("Error: index must be greater than 0"); fflush(stdout); return; }
 	while(temp->next!=NULL && index>0){
 		temp=temp->next;
 		index--;
@@ -83,25 +105,97 @@ void add_node_after_inx(ll* linked_list, int value, int index){
 		linked_list->count++;
 }
 
+int get_node_inx_by_val(ll* linked_list, int value){
+	element* temp= linked_list->start;
+	int ix=0;
+	while (temp->next!=NULL){
+	if (temp->value==value)
+		return ix;
+		ix++;
+		temp=temp->next;
+	}
+	if (temp->next==NULL)
+		if (temp->value==value){
+			ix++;
+			return ix;
+		}
+		return -1;
+	
+}
+
+void replace_node_by_value(ll* linked_list, int fromVal, int toVal){
+	element* temp= linked_list->start;
+	while (temp->next!=NULL){
+	if (temp->value==fromVal){
+		temp->value=toVal;
+		return;
+		}
+		temp=temp->next;
+	}
+	if (temp->next==NULL)
+		if (temp->value==fromVal){
+			temp->value=toVal;
+		}
+}
+
+void delete_node_by_value(ll* linked_list, int value){
+	element* temp= linked_list->start;
+	while (temp->next!=NULL){
+	if (temp->value==value){
+		temp->next->prev=temp->prev;
+		temp->prev->next=temp->next;
+		free(temp);
+		return;
+		}
+		temp=temp->next;
+	}
+	if (temp->next==NULL)
+		if (temp->value==value){
+			temp->prev->next=NULL;
+			return;
+		}
+}
+
+
+	
 
 
 int main(){
 	ll* linked_list = create_ll();
-	int choose, val, idx;
+	int choose, val, idx, toVal;
 	while(1==1){
-		printf("What do you want to do? 1-add an element, 2-print elements 3-print backwards: ");
+		printf("What do you want to do? 1-add an element, 2-print elements 3-print backwards, 4-Delete element at index, 5-Get first index by value, 6-replace values, 7-delete element by value: ");
 		scanf("%d", &choose);
 		switch(choose){
 			case 1:
 				printf("Input value of element and index you want to put it after: ");
 				scanf("%d %d", &val, &idx);
-				add_node_after_inx(linked_list, val, idx);
+				add_node_at_inx(linked_list, val, idx);
 				break;
 			case 2: print_ll(linked_list); printf("\n"); break;
 			case 3: print_ll_bck(linked_list); printf("\n"); break;
+			case 4: 
+				printf("Input index of element you want to delete: ");
+				scanf("%d", &idx);
+				del_node_at_inx(linked_list, idx);
+				break;
+			case 5:
+				printf("Input value you looking for: ");
+				scanf("%d", &val);
+				printf("Is at index %d", get_node_inx_by_val(linked_list, val));
+				break;
+			case 6:
+				printf("Input value you want to replace and value to replace with: ");
+				scanf("%d %d", &val, &toVal);
+				replace_node_by_value(linked_list, val, toVal);
+				break;
+			case 7:
+				printf("Input value to delete: ");
+				scanf("%d", &val);
+				delete_node_by_value(linked_list, val);
+				break;
 			default: printf("Wrong choice\n");
 		}
 		printf("\n\n\n\n");
 	}
 }
-
